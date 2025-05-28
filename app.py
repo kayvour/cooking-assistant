04c54f586ae255109cd0f87e9e4c15289720fe28
kayvour <vaidyakaivalya77@gmail.com>
2025-05-28 18:36:49 +0530

    changed home to index to deploy on github pages

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.secret_key = 'supersecretkey'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/assistant', methods=['GET', 'POST'])
 def assistant():
