f08b0c41e256e100a55e7228c47039830d89ba93
kayvour <vaidyakaivalya77@gmail.com>
2025-06-02 14:49:05 +0530

    edited paths

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import os
-from flask import Flask, request, redirect, url_for, session, send_file, render_template
+from flask import Flask, request, redirect, url_for, session, send_file, render_template, send_from_directory
 from recipe_scrapers import scrape_me
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
+# Static file serving routes for Vercel
+@app.route('/static/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static/css'), filename)
+
+@app.route('/static/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static/js'), filename)
+
+@app.route('/static/images/<path:filename>')
+def serve_images(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static/images'), filename)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
+
 @app.route('/')
 def index():
+    # You'll need to move index.html to templates/ folder for this to work
+    # Or keep using send_file if index.html is in root directory
     return send_file('index.html')
 
 @app.route('/assistant', methods=['GET', 'POST'])
